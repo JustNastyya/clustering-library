c2828cde818cdf0335367bea4ecc6921d441fe48
Anastasiia Romanenko <romanenko.a.e.04@gmail.com>
2025-03-02 23:30:58 +0100

    unsupervised clustering basics added

---
+++ unsupervised_clustering.py
@@ -0,0 +1,23 @@
+from data_points import DataPoints
+from labeled_data_points import LabeledDataPoints
+
+
+class UnsupervisedClusterer:
+    def __init__(self, data: DataPoints):
+        self.data = data
+    
+    # method = "single linkage", "complete linkage", "average linkage"
+    # returns LabeledDataPoints
+    def get_clusters(self, method: str):
+        # TODO define cluster class
+        while not(self._is_one_cluster()):
+            self._merge_closest(method)
+
+    def _is_one_cluster(self):
+        pass
+
+    def _merge_closest(self, method: str):
+        pass
+
+    def plot_dendogram(self): # TODO should i?
+        pass
