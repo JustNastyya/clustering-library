22a02ae4eb8986a374ea90f850b16893b7bfc29e
Anastasiia Romanenko <romanenko.a.e.04@gmail.com>
2025-03-02 23:05:54 +0100

    evaluation of data points added

--- data_points.py
+++ data_points.py
@@ -5,6 +5,7 @@ class DataPoints:
     # where p1 = [x1, x2, x3, ..., xk]
     # and len(p1) = len(p2) = len(p3) = ... = len(pn)
     def __init__(self, data: list):
+        self._evaluate_data_points(data)
         self.data = data
     
     def __len__(self):
@@ -15,3 +16,11 @@ class DataPoints:
 
     def __iter__(self):
         return iter(self.data)
+
+    def _evaluate_data_points(self, data_points: list):
+        for data_point in data_points:
+            if len(data_point) != len(data_points[0]):
+                raise ValueError("data points must have the same length")
+            if not(all(isinstance(x, (int, float)) for x in data_point)):
+                raise ValueError("data points must be of type int or float")
+        return True
