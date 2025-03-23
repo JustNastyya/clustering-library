06ab2a51a6ef03ffb373b03d22050e8a2e07295a
Anastasiia Romanenko <romanenko.a.e.04@gmail.com>
2025-03-23 18:35:24 +0100

    tests added

--- clustering/tests/test_02_k_means.py
+++ clustering/tests/test_02_k_means.py
@@ -1,10 +1,8 @@
 from clustering.basic_data_structures.data_points import DataPoints
 from clustering.basic_data_structures.labeled_data_points import LabeledDataPoints
 from clustering.method_classes.k_means import KMeans
-from clustering.method_classes.k_nn import KNN
-import matplotlib.pyplot as plt
 
-# TODO jypzter notebook
+
 def test_01():
     x = [4, 5, 10, 4, 3, 11, 14 , 8, 10, 12]
     y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
@@ -13,19 +11,6 @@ def test_01():
     knn = KMeans(data)
     points = knn.fit(k=3, max_iter=100)
     
-    plot_data(points)
-
-
-def plot_data(data: LabeledDataPoints):
-    plt.figure(figsize=(6, 6))
-    colors = ['blue', 'green', 'red', 'purple', 'orange', 'brown', 'pink', 'gray', 'olive', 'cyan']
-    
-    for key in data:
-        x_0 = [data[key][i][0] for i in range(len(data[key]))]
-        y_0 = [data[key][i][1] for i in range(len(data[key]))]
-
-        plt.scatter(x_0, y_0, color=colors[key])
-        
-    plt.show()
-
-
+    assert points.data[0].data == [[4, 21], [5, 19]]
+    assert points.data[1].data == [[4, 17], [3, 16]]
+    assert points.data[2].data == [[10, 24], [11, 25], [14, 24], [8, 22], [10, 21], [12, 21]]
